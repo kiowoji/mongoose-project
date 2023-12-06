72ab6691bff272349a297bc908948ec64be9f438
Iryna Murzak <iramurzak@gmail.com>
2023-12-06 16:03:35 +0200

    create user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,70 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-
+  firstName: {
+    type: String,
+    minlength: 4,
+    maxlength: 50,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    minlength: 3,
+    maxlength: 60,
+    required: true,
+    trim: true,
+  },
+  fullName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: (email) => {
+        const re = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+        return re.test(email);
+      },
+      message: "Please enter a valid email.",
+    },
+  },
+  role: {
+    type: String,
+    enum: ["admin", "writer", "guest"],
+  },
+  age: {
+    type: Number,
+    min: 1,
+    max: 99,
+  },
+  numberOfArticles: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", function (next) {
+  this.fullName = `${this.firstName} ${this.lastName}`;
+  if (this.age < 0) this.age = 1;
+  next();
+});
+
+userSchema.pre("updateOne", function (next) {
+  this.getUpdate().updatedAt = new Date();
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
