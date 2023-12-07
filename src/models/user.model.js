0e75e777b1f41b9c4f76b7b42082d08c751a4cc9
Iryna Murzak <iramurzak@gmail.com>
2023-12-07 13:11:24 +0200

    add the ability to like the article by the user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,6 +50,12 @@ const userSchema = new mongoose.Schema({
       ref: "Article",
     },
   ],
+  likedArticles: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Article",
+    },
+  ],
   createdAt: {
     type: Date,
     default: Date.now,
